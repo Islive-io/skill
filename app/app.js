f0316255e80143df13aa72db84e06015000ebb41
Deltanic <rdg94@live.nl>
2014-09-16 17:29:39 +0200

    Whoops

---
+++ app/app.js
@@ -0,0 +1,69 @@
+var express  = require("express");
+var socketio = require("socket.io");
+var http 	 = require("http");
+var path 	 = require("path");
+
+var APP_OPTIONS = {
+	serverPort: 80,
+    assetPath: path.join(__dirname, "/assets"),
+	viewOptions: {
+		root: path.join(__dirname, "/templates/views/")
+	}
+};
+
+// ----------------------------------------------------------------------
+
+function ChatSystemAPI() {
+
+}
+
+ChatSystemAPI.prototype.run = function() {
+
+};
+
+var api = new ChatSystemAPI();
+
+api.run();
+
+// ----------------------------------------------------------------------
+
+var app    = express();
+var server = http.Server(app);
+var io     = socketio(server);
+var log    = [];
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    // Awesome database.
+    for (var i = 0; i < log.length; i++) {
+        socket.emit("onMessage", log[i]);
+    }
+
+    socket.on("onMessage", function(messageObject) {
+        console.log(messageObject.nickname + ": " + messageObject.message);
+
+        log.push(messageObject);
+
+        while (log.length > 10) {
+            log.shift();
+        }
+
+        io.emit("onMessage", messageObject);
+    });
+});
+
+
+
+app.use("/assets", express.static(APP_OPTIONS.assetPath));
+
+app.get("/", function (request, response) {
+	response.sendFile("index.html", APP_OPTIONS.viewOptions);
+});
+
+server.listen(APP_OPTIONS.serverPort, function () {
+	console.log("Server running on localhost: " + APP_OPTIONS.serverPort);
+});
+
+
+
