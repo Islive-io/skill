7860ddf359e67c0b389bc3f924bbfde4899e50eb
Wojtek Dmyszewicz <wdmy85@gmail.com>
2014-10-09 22:40:20 +0200

    - initialize a new instance of socket.io by passing the http (the HTTP server) object - listen on the connection event for incoming sockets - log new user to the console - load the socket.io-client, which exposes a io global in index.html

--- index.js
+++ index.js
@@ -3,9 +3,14 @@
 var app = require('express')();
 var serveStatic = require('serve-static');
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(request, response) {
-    response.sendFile(__dirname + "/public/views/index.html")
+    response.sendFile(__dirname + "/public/views/index.html");
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
 });
 
 http.listen(3000, function(){
