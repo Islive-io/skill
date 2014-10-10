262e97ebce2a1e4ea6dfb5eae26f14ba545c9e92
Wojtek Dmyszewicz <wdmy85@gmail.com>
2014-10-10 03:09:29 +0200

    - trying to get app.js working instead of index.js - changed color form buttons

--- public/js/app.js
+++ public/js/app.js
@@ -1,60 +1,35 @@
-var express = require('express');
+var app = require('express')();
+var serveStatic = require('serve-static');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+app.use(serveStatic(path.join(__dirname, 'public')));
 
+// TODO: get routes working with view engine
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', '{views}');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', '{views}');
+//app.use('/', routes);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());{css}
-app.use(express.static(path.join(__dirname, '/public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+/* GET home page. */
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+io.on('connection', function(socket){
+    io.emit('new user', 'NEW USER LOGGED IN');
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
     });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+    socket.on('new user', function(msg){
+        io.emit('new user', msg);
     });
 });
 
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
 
-module.exports = app;
+//module.exports = app;
